12d4f924fdc5e19cc4e9f008e7b14dd7850ed2e4
Ryohei <rswt1018@gmail.com>
2026-06-24 20:16:32 +0900

    Clarify public repository footer link

--- public/secret-mission/sw.js
+++ public/secret-mission/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "last-war-secret-mission-2026-06-24-23";
+const CACHE_NAME = "last-war-secret-mission-2026-06-24-24";
 const APP_SHELL = [
   "./",
   "./index.html",
--- src/App.tsx
+++ src/App.tsx
@@ -82,7 +82,7 @@ import {
 } from "@/lib/i18n";
 import { cn } from "@/lib/utils";
 
-const appVersion = "2026-06-24-23";
+const appVersion = "2026-06-24-24";
 const excludedServersCookieName = "lastwar-secret-mission-excluded-servers";
 const appBasePath = "/secret-mission/";
 const authorPagePath = "/secret-mission/author/";
